96f8740917024fe7d42a32ea93e9292f48eec181
123456 <2505961012@qq.com>
2019-10-11 21:25:16 +0800

    Sacla

---
+++ src/one/hello.scala
@@ -0,0 +1,16 @@
+package one
+
+class hello {
+
+    def  main(args: Array[String]): Unit = {
+      println("hello is in one pakge");
+    }
+
+
+
+}
+object al{
+  def  main(args: Array[String]): Unit = {
+    println("hello is in one pakge");
+  }
+}
